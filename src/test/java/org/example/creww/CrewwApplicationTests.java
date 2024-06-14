feacdc3651fce91ff3efbb1e069b18e365d5c9c3
pie0902 <zhfoqkq@gmail.com>
2024-06-14 16:35:31 +0900

    ver

--- src/test/java/org/example/creww/CrewwApplicationTests.java
+++ src/test/java/org/example/creww/CrewwApplicationTests.java
@@ -3,7 +3,6 @@ package org.example.creww;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
 
 @SpringBootTest
